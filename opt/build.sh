4f35f0e3f6724d43d9991629fbc53ba46ed7e1de
Ekrem BAL <mail.ekrembal@gmail.com>
2025-09-25 21:52:47 +0300

    Revert "Custom embit"

--- opt/build.sh
+++ opt/build.sh
@@ -5,8 +5,8 @@ set -o errexit -o pipefail
 # global variables
 cur_dir_name=${PWD##*/}
 cur_dir=$(pwd)
-seedsigner_app_repo="https://github.com/ekrembal/seedsigner.git"
-seedsigner_app_repo_branch="show_sighash_and_psbt_hash"
+seedsigner_app_repo="https://github.com/SeedSigner/seedsigner.git"
+seedsigner_app_repo_branch="dev"
 
 help()
 {
